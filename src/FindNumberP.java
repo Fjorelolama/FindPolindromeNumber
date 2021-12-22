425ae3538e9940b8bb9f9039c7a82b43e4dd76a3
Fjorelolama <82113469+Fjorelolama@users.noreply.github.com>
2021-12-22 13:11:01 +0100

    Initial commit

---
+++ src/FindNumberP.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+
+public class FindNumberP {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter a Number from 101 to 535: ");
+        int Number_p = userInput.nextInt();
+
+        int Poli_NUm1 = 101;
+        int Poli_NUm2 = 111;
+        int Poli_NUm3 = 121;
+        int Poli_NUm4 = 131;
+        int Poli_NUm5 = 141;
+        int Poli_NUm6 = 151;
+        int Poli_NUm7 = 161;
+        int Poli_NUm8 = 171;
+        int Poli_NUm9 = 181;
+        int Poli_NUm10 = 191;
+        int Poli_NUm11 = 303;
+        int Poli_NUm12 = 313;
+        int Poli_NUm13 = 333;
+        int Poli_NUm14 = 343;
+        int Poli_NUm15 = 353;
+        int Poli_NUm16 = 363;
+        int Poli_NUm17 = 373;
+        int Poli_NUm18 = 383;
+        int Poli_NUm19 = 393;
+        int Poli_NUm20 = 404;
+        int Poli_NUm21 = 414;
+        int Poli_NUm22 = 434;
+        int Poli_NUm23 = 444;
+        int Poli_NUm24 = 454;
+        int Poli_NUm25 = 464;
+        int Poli_NUm26 = 474;
+        int Poli_NUm27 = 484;
+        int Poli_NUm28 = 494;
+        int Poli_NUm29 = 505;
+        int Poli_NUm30 = 515;
+        int Poli_NUm31 = 535;
+
+        if (Number_p == Poli_NUm1) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm2) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm3) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm4) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm5) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm6) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm7) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm8) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm9) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm10) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm11) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm12) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm13) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm14) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm15) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm16) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm17) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm18) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm19) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm20) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm21) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm22) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm23) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm24) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm25) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm26) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm27) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm28) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm29) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm30) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else if (Number_p == Poli_NUm31) {
+            System.out.println(Number_p + " Is a Polindrome number!  Good Job.");
+        } else {
+            System.out.println(Number_p + " Sorry it isn't a Polindrome Number");
+        }
+
+
+    }
+}
